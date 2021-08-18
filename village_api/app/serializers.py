4e825e4d4b5f7c05cce769487dcf8aaeaccbd2df
Rose Dickinson <rose.dickinson@stfc.ac.uk>
2021-08-18 20:02:46 +0100

    feat: Add people to locations

--- village_api/app/serializers.py
+++ village_api/app/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from village_api.models import Person, Location, Relationship
@@ -16,13 +17,27 @@ class RelationshipSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class LocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Location
-        fields = "__all__"
-
-
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = "__all__"
+
+
+class PersonBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ["id", "name", "status", "parents", "siblings"]
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    people = serializers.SerializerMethodField("get_people")
+
+    class Meta:
+        model = Location
+        fields = "__all__"
+
+    @swagger_serializer_method(serializer_or_field=PersonBasicSerializer(many=True))
+    def get_people(self, instance):
+        people = Person.objects.filter(location=instance.id)
+
+        return PersonBasicSerializer(people, many=True).data
